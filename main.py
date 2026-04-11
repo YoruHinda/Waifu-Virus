f162c910948056b0542987b433887ba8d4168e5f
Ruan Phablo <ruanphablo778@gmail.com>
2026-04-10 22:51:24 -0300

    Feat: Download Images From Api

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import os
 import requests
 
+def downloadImages(folder = "links"):
+    os.makedirs(folder, exist_ok = True)
+
+    for i in range(5):
+        response = requests.get("https://api.waifu.im/images?IsNsfw=True")
+        response.raise_for_status()
+
+        data = response.json()
+        image = data["items"][0]
+
+        url = image["url"]
+        archive_name = url.split("/")[-1]
+        path = os.path.join(folder, archive_name)
+
+        img_response = requests.get(url, stream= True)
+        img_response.raise_for_status()
+
+        with open(path, "wb") as f:
+            for chunk in img_response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print("salvo")
+
+downloadImages()
